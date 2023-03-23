5057ddbef3aef592435c4548d965790cb92c44e1
MattZKruspe <matthewmarquez2003@gmail.com>
2023-03-23 12:00:35 -0600

    fifth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:componentes/ui/screens/alerts.dart';
 import 'package:componentes/ui/screens/home_page_temp.dart';
+import 'package:componentes/ui/screens/input_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -17,6 +18,8 @@ class MyApp extends StatelessWidget {
       routes: <String, WidgetBuilder>{
         '/': (BuildContext context) => const HomePageTemp(),
         'alert' : (BuildContext context) => const AlertsScreen(),
+        'inputs' : (BuildContext context) => const InputsScreen(),
+
       },
       onGenerateRoute: (RouteSettings settings){
         print('Ruta llamada: ${settings.name}');
--- lib/ui/screens/input_screen.dart
+++ lib/ui/screens/input_screen.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
-class InputScreen extends StatelessWidget {
-  const InputScreen({super.key});
+class InputsScreen extends StatefulWidget {
+  const InputsScreen({super.key});
 
   @override
+  State<InputsScreen> createState() => _InputsScreenState();
+}
+
+class _InputsScreenState extends State<InputsScreen> {
+  String _userName = '';
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Input Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Entradas de datos por el usuario'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: [
+          _crearEntrada(),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearEntrada() {
+    return TextField(
+      autofocus: true,
+      textCapitalization: TextCapitalization.words,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+      onChanged: (valor) {
+        _userName = valor;
+        print(_userName);
+      },
     );
   }
 }
