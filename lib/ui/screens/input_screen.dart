961c08ccdad780e566a2a7c00a50c950bd5f3c4b
MattZKruspe <matthewmarquez2003@gmail.com>
2023-03-27 14:01:48 -0600

    sixth commit

--- lib/ui/screens/input_screen.dart
+++ lib/ui/screens/input_screen.dart
@@ -19,6 +19,7 @@ class _InputsScreenState extends State<InputsScreen> {
         padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: [
           _crearEntrada(),
+          _crearEntradaPswd()
         ],
       ),
     );
@@ -27,16 +28,70 @@ class _InputsScreenState extends State<InputsScreen> {
   Widget _crearEntrada() {
     return TextField(
       autofocus: true,
-      textCapitalization: TextCapitalization.words,
+      textAlign: TextAlign.left,
+      textCapitalization: TextCapitalization.sentences,
+      style: const TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.bold, 
+        ),
+
+        cursorColor: const Color.fromARGB(255, 0, 0, 0),
+        cursorRadius: const Radius.circular(10.0),
+        cursorWidth: 1.0,
+        maxLength: 10,
+        obscureText: true,
       decoration: InputDecoration(
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-      ),
+          borderRadius: BorderRadius.circular(20.0)),
+          hintText: "Enter name",
+          labelText: "Name: ",
+
+          // prefix: const CircularProgressIndicator(),
+
+          icon: Icon(Icons.person),
+          iconColor: Color.fromARGB(255, 13, 113, 152),
+          ),
       onChanged: (valor) {
         _userName = valor;
         print(_userName);
       },
     );
   }
+
+    Widget _crearEntradaPswd() {
+    return TextField(
+      autofocus: true,
+      textAlign: TextAlign.left,
+      textCapitalization: TextCapitalization.sentences,
+      style: const TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.bold, 
+        ),
+        cursorColor: const Color.fromARGB(255, 0, 0, 0),
+        cursorRadius: const Radius.circular(10.0),
+         cursorWidth: 1.0,
+        maxLength: 10,
+        obscureText: true,
+        keyboardType: TextInputType.visiblePassword,
+        // maxLength: 2,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)),
+          hintText: "Enter password",
+          labelText: "Password: ",
+
+          // prefix: const CircularProgressIndicator(),
+
+          icon: Icon(Icons.password),
+          iconColor: Color.fromARGB(255, 13, 113, 152),
+          ),
+      onChanged: (valor) {
+        _userName = valor;
+        print(_userName);
+      },
+    );
+  }
+
+
+
 }
